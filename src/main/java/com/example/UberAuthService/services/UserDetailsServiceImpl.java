1ff95b3c7fa7e7f755911d6bb041ee27317c6881
Pratham-Anand <anandpratham13@gmail.com>
2025-07-14 23:54:35 +0530

    added userdetails class and its impl

---
+++ src/main/java/com/example/UberAuthService/services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.UberAuthService.services;
+
+
+import com.example.UberAuthService.helpers.AuthPassengerDetails;
+import com.example.UberAuthService.models.Passenger;
+import com.example.UberAuthService.repositories.PassengerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private  final PassengerRepository passengerRepository;
+
+    public UserDetailsServiceImpl(PassengerRepository passengerRepository){
+        this.passengerRepository=passengerRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger=passengerRepository.findPassengerByEmail(email);     //email is the username here.
+
+        if(passenger.isPresent()){
+            return new AuthPassengerDetails(passenger.get());
+        }
+        else{
+            throw new UsernameNotFoundException("Cannot find the Passenger by the given Email");
+        }
+    }
+}
